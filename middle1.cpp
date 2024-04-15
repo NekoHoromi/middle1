4fb17048a5e0a83a2e9d4c27b5777513281ab28b
Хороми <128912298+NekoHoromi@users.noreply.github.com>
2024-04-15 13:33:13 +0300

    Update middle1.cpp

---
+++ middle1.cpp
@@ -0,0 +1,69 @@
+#include "middle.h"
+using namespace std;
+
+void itc_num_print(int number){
+    cout << number << endl;
+}
+int itc_len_num(long long number){
+    if (number <= 0) {
+        return -1;
+    }
+    int kol = 0;
+    while (number > 0){
+        number /= 10;
+        kol ++;
+    }
+    return kol;
+}
+int itc_sum_num(long long number){
+    if (number <= 0){
+        return -1;
+    }
+    int number2, sum = 0;
+    while (number > 0){
+        number2 = number % 10;
+        sum += number2;
+        number /= 10;
+    }
+    return sum;
+}
+long long itc_multi_num(long long number){
+    if (number <= 0){
+        return -1;
+    }
+    int number2, proz = 1;
+    while (number > 0){
+        number2 = number % 10;
+        proz *= number2;
+        number /= 10;
+    }
+    return proz;
+}
+int itc_max_num(long long number){
+    if (number <= 0){
+        return -1;
+    }
+    int number2, mmax = 0;
+    while (number > 0){
+        number2 = number % 10;
+        if (number2 > mmax){
+            mmax = number2;
+        }
+        number /= 10;
+    }
+    return mmax;
+}
+int itc_min_num(long long number){
+    if (number <= 0) {
+        return -1;
+    }
+    int number2, mmin = 9;
+    while (number > 0){
+        number2 = number % 10;
+        if (number2 < mmin){
+            mmin = number2;
+        }
+        number /= 10;
+    }
+    return mmin;
+}
